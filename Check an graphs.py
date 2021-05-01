55c7a6cbed452e18d2608eead5642eb28424e88c
Bevanje <56934698+Bevanje@users.noreply.github.com>
2021-04-30 20:10:11 -0400

    Add files via upload

--- Check an graphs.py
+++ Check an graphs.py
@@ -15,11 +15,29 @@ print(dataset['Games Lost'].sum())
 t1 = list(dataset["CurrentSR"])
 t2 = list(dataset["StartingSR"])
 
-plt.hist(t2, 500 ,facecolor='red',alpha=1, label ="Starting SR")
-plt.hist(t1, 500,facecolor='black',alpha=1, label = "Current SR")
+"""
+plt.hist(t2, 250 ,facecolor='red',alpha=1, label ="Starting SR")
+plt.hist(t1, 250,facecolor='black',alpha=1, label = "Current SR")
 plt.xlabel("Players SR")
 plt.ylabel("Number of players")
 plt.legend(loc="upper right")
 
+"""
 
-#plt.savefig('filename.png', dpi=300)
+
+
+
+t3 = [None] *5000
+
+num = 0
+for x in t2:
+    t3[num] = x - t1[num]
+    num = num + 1
+    
+plt.scatter(t3,t2, alpha = 0.25, s=3.5, color="orange")
+plt.xlabel("SR Delta")
+plt.ylabel("Starting SR")
+
+
+
+plt.savefig('filen6ame.png', dpi=300)
